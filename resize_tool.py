fe4d86ce1eebf6c2c22ebd8c2f3f2dc396952729
rese1f <reselfchai@gmail.com>
2023-01-03 20:35:05 +0800

    reduce resolution

--- resize_tool.py
+++ resize_tool.py
@@ -7,10 +7,11 @@ source_folder = './gallery_full_resolution'
 destination_folder = './gallery'
 
 # 5472 x 3648 (3:2)
+# 720p: 1280 x 853.33
 # 1080p: 1920 x 1280
 # 2k: 2560 x 1706.66
 
-size = (2560, 1707)
+size = (1280, 854)
 
 if os.path.exists(destination_folder):
     shutil.rmtree(destination_folder)
